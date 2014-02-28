9daadcfe5ae1a8200cc1d4649b3be08dd4289d26
Chakaitos <chakaitos@gmail.com>
2014-02-28 12:55:12 -0500

    Complete exercises 1-5

--- ruby/ruby_classes_2/cars.rb
+++ ruby/ruby_classes_2/cars.rb
@@ -1,11 +1,24 @@
 class Car
+	attr_accessor :cars_per_color
+
+	@@total_car_count = 0
+	@@cars_per_color = {}
+	
+
 	def to_s()
-		"I'm a car! I've driven #{@distance} and have #{@fuel} gallons gas left"
+		"I'm a #{@color} car! I've driven #{@distance} and have #{@fuel} gallons gas left"
 	end
 
-	def initialize()
+	def initialize(color='yellow')
+		@color = color
 		@fuel = 10
 		@distance = 0
+
+		if @@cars_per_color.has_key?(color)
+			@@cars_per_color[color] += 1
+		else
+			@@cars_per_color[color] = 1 
+		end
 	end
 
 	def drive(miles)
@@ -24,16 +37,43 @@ class Car
 		puts "You must pay $#{3.5 * gallons_needed}"
 		@fuel = 10.0
 	end
+
+	def color=(color)
+		@color = color
+	end
+
+	def self.most_popular_color
+		@@cars_per_color.select { |k, v| v == @@cars_per_color.values.max }.keys.join
+	end
+
+	def self.total_car_count
+		@@total_car_count
+	end
+
+	def self.cars_per_color
+		@@cars_per_color
+	end
 end
 
-car_a = Car.new()
-car_b = Car.new()
-puts car_a
-puts car_b
-car_a.drive(10)
-puts car_a
-puts car_b
-car_a.drive(232)
-car_b.drive(117)
-puts car_a
-puts car_b
+car_a = Car.new("blue")
+car_b = Car.new("red")
+car_c = Car.new("red")
+car_d = Car.new
+car_e = Car.new("red")
+# puts car_a
+# puts car_b
+# car_a.drive(10)
+# puts car_a
+# puts car_b
+# car_a.drive(232)
+# car_b.drive(117)
+# puts car_a
+# puts car_b
+# puts Car.cars_per_color
+# puts Car.cars_per_color.keys
+# puts Car.most_popular_color
+best_color = Car.most_popular_color
+puts best_color
+car_f = Car.new(best_color)
+puts Car.cars_per_color
+puts car_f
